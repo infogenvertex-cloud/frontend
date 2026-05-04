54052e5f2a5209db53d9a51e360207f9b1cf31ce
subbu6149 <udasubash143@gmail.com>
2026-05-04 14:55:18 +0530

    Use environment variable for API URL in axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-gamma-seven-22.vercel.app",
+  baseURL: import.meta.env.VITE_API_URL || "https://backend-gamma-seven-22.vercel.app",
 });
 
 api.interceptors.request.use((config) => {
